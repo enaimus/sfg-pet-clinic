9a219db64c3ec051569a6c987010b66b36e81e8f
enaimus <c.lopez.jav@gmail.com>
2020-02-18 11:28:45 +0100

    Map id generation complete. Closes #24.

--- pet-clinic-data/src/main/java/com/enaimus/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/enaimus/sfgpetclinic/services/map/AbstractMapService.java
@@ -4,30 +4,33 @@ import com.enaimus.sfgpetclinic.models.BaseEntity;
 
 import java.util.*;
 
-public abstract class AbstractMapService <T extends BaseEntity, ID extends Long>{
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<Long ,T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
-    Set<T> findAll(){
+    Set<T> findAll() {
         return new HashSet<>(map.values());
 
     }
 
-    T findById(ID id){
+    T findById(ID id) {
         return map.get(id);
     }
 
-    T save (ID id, T object){
-        if(object != null){
-            if (object.getId()==null){
-            object.setId(getNextId());
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
         }
-        map.put(id, object);
+        else{
+            throw new RuntimeException("New object can't be null");
         }
         return object;
     }
 
-    void removeById(ID id){
+    void removeById(ID id) {
         map.remove(id);
     }
 
@@ -35,8 +38,15 @@ public abstract class AbstractMapService <T extends BaseEntity, ID extends Long>
         map.entrySet().removeIf(x -> x.equals(object));
     }
 
-    private Long getNextId(){
-        return Collections.max(map.keySet()) + 1;
+    private Long getNextId() {
+        Long nextId = null;
+        if (map.isEmpty()){
+            nextId = 1L;
+        }
+        else{
+            nextId =Collections.max(map.keySet()) + 1;
+        }
+        return nextId;
     }
 
 }
