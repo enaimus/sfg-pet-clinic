c03ddb24964eb1ed43c86e766bdf56746fc0b0cb
enaimus <c.lopez.jav@gmail.com>
2020-02-26 12:07:44 +0100

    Add contact info to Owner. Closes #29.

---
+++ pet-clinic-data/src/main/java/com/enaimus/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.enaimus.sfgpetclinic.services;
+
+import com.enaimus.sfgpetclinic.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/enaimus/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,33 @@
+package com.enaimus.sfgpetclinic.services.map;
+
+import com.enaimus.sfgpetclinic.models.PetType;
+import com.enaimus.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.remove(petType);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.removeById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
